c867b745c64111cd731153fc2496014a5a7b5518
J1aM1ng <2605379072@qq.com>
2022-03-21 22:34:25 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import Content from './components/content/Content';
+import Footer from './components/content/Footer';
+import Nav from './components/Nav/Nav';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
